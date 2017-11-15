d95f19183dbf3e1f9667fdb27c7a7f251f4fdde6
Maciej Życiski <maciej.zycinski@lunapolska.pl>
2017-11-15 09:04:34 +0100

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+  
+	void Start () {
+
+        Text textscript = GameObject.Find("Title").GetComponentInChildren<Text>(); // This will get the script responsable for editing text
+        textscript.text = "Podaj pierwszą litere wyrazu jabłko";
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+      
+    }
+}
---
+++ Assets/onclick.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class onclick : MonoBehaviour {
+
+    public ColorBlock colorBlockHuman;
+ public int licznik ;
+    public void Wypiszlitere(Button button)
+    {
+        if (button.name=="Restart")
+        {
+            SceneManager.LoadScene("Scena");
+        }
+
+        licznik++;
+        Debug.Log(licznik);
+        Text tiltlet = GameObject.Find("Title").GetComponentInChildren<Text>();
+        tiltlet.color = Color.black;
+          
+        if (licznik == 2)
+        {
+            tiltlet.text = "Podaj ostatnią literę";
+        }
+       else if(licznik>2)
+        {
+            tiltlet.text = "Wygrałes";
+        }
+        else
+        {
+            tiltlet.text = "Podaj nastepną literę";
+        }
+        ColorBlock colorVar = button.colors;
+        colorVar.highlightedColor = Color.black;
+        colorVar.normalColor= Color.black;
+        button.colors = colorVar;
+        if (licznik <= 2)
+        {
+            Text textscript = GameObject.Find("T" + licznik).GetComponentInChildren<Text>(); // This will get the script responsable for editing text
+
+
+            textscript.text = button.name;
+            textscript.enabled = false;
+            textscript.enabled = true;
+            textscript.color = Color.black;
+
+           
+        }
+        
+    }
+}
