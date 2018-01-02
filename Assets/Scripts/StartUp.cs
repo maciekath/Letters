be9075c8895de574b2e0a7d8f30acb26bc7e44c1
Maciej Życiski <maciej.zycinski@lunapolska.pl>
2018-01-02 11:06:55 +0100

    dodanie nowego wyrazu oliwka

--- Assets/Scripts/StartUp.cs
+++ Assets/Scripts/StartUp.cs
@@ -13,11 +13,7 @@ public class StartUp : MonoBehaviour {
 
     void Start() {
 
-        for (int i = 0; i < sprites.Count; i++)
-        {
-            Debug.Log(sprites[i].name);
 
-        }
         InputField input = GameObject.Find("templemate").GetComponentInChildren<InputField>();
         var ObjectToDeactivate = GameObject.Find("templemate").GetComponentInChildren<InputField>();
 
@@ -26,23 +22,28 @@ public class StartUp : MonoBehaviour {
         g.gameObject.SetActive(false);
         var g1 = GameObject.Find("j").GetComponentInChildren<Image>();
         g1.gameObject.SetActive(false);
+        var g2 = GameObject.Find("o").GetComponentInChildren<Image>();
+        g2.gameObject.SetActive(false);
         System.Random rnd = new System.Random();
-        int losowanie = rnd.Next(0, 2);
+        int losowanie = rnd.Next(0, 3);
         los = losowanie;
         string w = "JABLKO";
         string gruszka = "GRUSZKA";
+        string oliwka = "OLIWKA";
 
         Wyraz w0 = new Wyraz(w);
         Wyraz w1 = new Wyraz(gruszka);
-
+        Wyraz w2 = new Wyraz(oliwka);
         w0.dodajdolisty();
 
         w1.dodajdolisty();
-
+        w2.dodajdolisty();
         if (!slownik.ContainsKey(w))
             slownik.Add(w, w0);
         if (!slownik.ContainsKey(gruszka))
             slownik.Add(gruszka, w1);
+        if (!slownik.ContainsKey(oliwka))
+            slownik.Add(oliwka, w2);
         if (losowanie == 0)
         {
             g1.gameObject.SetActive(true);
@@ -51,6 +52,10 @@ public class StartUp : MonoBehaviour {
         {
 
             g.gameObject.SetActive(true);
+        }
+        if (losowanie==2)
+        {
+            g2.gameObject.SetActive(true);
         }
         Text textscript = GameObject.Find("Title").GetComponentInChildren<Text>(); // This will get the script responsable for editing text
         textscript.text = "Podaj pierwszą litere wyrazu ";
@@ -87,6 +92,21 @@ public class StartUp : MonoBehaviour {
 
                 }
                 break;
+            case 2:
+                for (int i = 1; i < oliwka.Length + 1; i++)
+                {
+
+
+                    litery.Add(Instantiate<InputField>(input, input.transform.parent));
+                    InputField field = GameObject.Find("templemate(Clone)").GetComponentInChildren<InputField>();
+                    field.transform.localPosition = new Vector3(-150 + przesuniecie, -100, 0);
+                    field.name = "F" + i;
+
+                    przesuniecie += 30;
+
+                }
+                break;
+
         }
         ObjectToDeactivate.gameObject.SetActive(false);
 
